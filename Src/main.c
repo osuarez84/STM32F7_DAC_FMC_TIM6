a3400790433aca4d8ecc5211b192a37c65dc80a4
Omar Suarez <vegetali@gmail.com>
2017-03-21 17:30:23 +0100

    WARNINGgit status Added one line that was missing from the code generated by the STM32CubeMX inside the SystemClock_Config function. The HSIState variable was not configuredgit status MCU was not able to boot properly. Added => RCC_OscInitStruct.HSIState = RCC_HSI_ON;

--- Src/main.c
+++ Src/main.c
@@ -135,7 +135,7 @@ void SystemClock_Config(void)
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-	//RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 25;
